606a6fbf8f06100a666af67f62f2a369781bf629
lomik31 <lomik31@codead.dev>
2023-03-30 18:17:34 +0300

    Функционал выставления оценок перенесен из Mentor в Reviewer

--- students_and_mentor.py
+++ students_and_mentor.py
@@ -12,7 +12,11 @@ class Mentor:
         self.name = name
         self.surname = surname
         self.courses_attached = []
-        
+
+class Lecturer(Mentor):
+    pass
+
+class Reviewer(Mentor):
     def rate_hw(self, student: Student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
             if course in student.grades:
@@ -22,11 +26,6 @@ class Mentor:
         else:
             return 'Ошибка'
 
-class Lecturer(Mentor):
-    pass
-
-class Reviewer(Mentor):
-    pass
  
 # best_student = Student('Ruoy', 'Eman', 'your_gender')
 # best_student.courses_in_progress += ['Python']
