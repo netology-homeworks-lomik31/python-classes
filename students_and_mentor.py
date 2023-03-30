fbfe6130efda4e19f6b4e998e9990213d0bbe3f1
lomik31 <lomik31@codead.dev>
2023-03-30 18:25:21 +0300

    Реорганизация классов + добавление метода выставления оценок лекторам от студентов

--- students_and_mentor.py
+++ students_and_mentor.py
@@ -1,3 +1,14 @@
+class Mentor:
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+        self.courses_attached = []
+
+class Lecturer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.grades = {}
+
 class Student:
     def __init__(self, name, surname, gender):
         self.name = name
@@ -6,16 +17,16 @@ class Student:
         self.finished_courses = []
         self.courses_in_progress = []
         self.grades = {}
+    
+    def rate_hw(self, lecturer: Lecturer, course, grade):
+        if isinstance(lecturer, Lecturer) and course in self.courses_in_progress and course in lecturer.courses_attached:
+            if course in lecturer.grades:
+                lecturer.grades[course] += [grade]
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            return 'Ошибка'
         
-class Mentor:
-    def __init__(self, name, surname):
-        self.name = name
-        self.surname = surname
-        self.courses_attached = []
-
-class Lecturer(Mentor):
-    pass
-
 class Reviewer(Mentor):
     def rate_hw(self, student: Student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
