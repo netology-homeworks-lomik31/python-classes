e49898c380f493f821da125abb98a41a7209210e
lomik31 <lomik31@codead.dev>
2023-03-30 18:09:45 +0300

    Добавлен класс лекторов и проверяющих

--- students_and_mentor.py
+++ students_and_mentor.py
@@ -13,7 +13,7 @@ class Mentor:
         self.surname = surname
         self.courses_attached = []
         
-    def rate_hw(self, student, course, grade):
+    def rate_hw(self, student: Student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
             if course in student.grades:
                 student.grades[course] += [grade]
@@ -21,15 +21,21 @@ class Mentor:
                 student.grades[course] = [grade]
         else:
             return 'Ошибка'
+
+class Lecturer(Mentor):
+    pass
+
+class Reviewer(Mentor):
+    pass
  
-best_student = Student('Ruoy', 'Eman', 'your_gender')
-best_student.courses_in_progress += ['Python']
+# best_student = Student('Ruoy', 'Eman', 'your_gender')
+# best_student.courses_in_progress += ['Python']
  
-cool_mentor = Mentor('Some', 'Buddy')
-cool_mentor.courses_attached += ['Python']
+# cool_mentor = Mentor('Some', 'Buddy')
+# cool_mentor.courses_attached += ['Python']
  
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
+# cool_mentor.rate_hw(best_student, 'Python', 10)
+# cool_mentor.rate_hw(best_student, 'Python', 10)
+# cool_mentor.rate_hw(best_student, 'Python', 10)
  
-print(best_student.grades)
+# print(best_student.grades)
