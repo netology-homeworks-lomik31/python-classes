a81f8d7e126e07f11a84ef1e728a33326ebd8141
lomik31 <lomik31@codead.dev>
2023-03-30 19:19:01 +0300

    Переопределены методы __str__ у Lecturer, Student и Reviewer

--- students_and_mentor.py
+++ students_and_mentor.py
@@ -8,7 +8,10 @@ class Lecturer(Mentor):
     def __init__(self, name, surname):
         super().__init__(name, surname)
         self.grades = {}
-    
+
+    def __str__(self) -> str:
+        return f"Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за лекции: {self.avg_grade()}"
+
     def avg_grade(self):
         grades = sum(map(lambda i: i, self.grades.values()), [])
         return f"{sum(grades)/len(grades):.1f}"
@@ -21,7 +24,11 @@ class Student:
         self.finished_courses = []
         self.courses_in_progress = []
         self.grades = {}
-    
+
+    def __str__(self) -> str:
+        return f"Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за домашние задания: {self.avg_grade()}\
+Курсы в процессе изучения: {', '.join(self.courses_in_progress)}\nЗавершенные курсы: {', '.join(self.finished_courses)}"
+
     def rate_hw(self, lecturer: Lecturer, course, grade):
         if isinstance(lecturer, Lecturer) and course in self.courses_in_progress and course in lecturer.courses_attached and isinstance(grade, int) and grade > 0 and grade <= 10:
             if course in lecturer.grades:
@@ -45,6 +52,8 @@ class Reviewer(Mentor):
         else:
             return 'Ошибка'
 
+    def __str__(self) -> str:
+        return f"Имя: {self.name}\nФамилия: {self.surname}"
  
 # best_student = Student('Ruoy', 'Eman', 'your_gender')
 # best_student.courses_in_progress += ['Python']
